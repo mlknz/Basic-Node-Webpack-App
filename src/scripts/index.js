cc499aaa3bddb26fb2cfba8cd52c77f9397b5fcc
mlknz <rkdshe@gmail.com>
2016-09-10 00:23:27 +0300

    clean up

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,8 +2,6 @@ import TestModule from './testModule';
 
 class App {
     constructor() {
-        // const config = require('./config');
-        // if (!config.webglEnabled) { do smth }
         this.test = new TestModule('123 message');
         this.test.foo();
     }
